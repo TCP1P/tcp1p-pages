14baca4eb677b04207e3a24de785b84ff95f5bb4
dimas <dimasmaulana0305@gmail.com>
2024-05-01 15:14:20 +0800

    change base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/",
-    assetPrefix: "/",
+    basePath: "",
+    assetPrefix: "",
     output: "export",
     images:{
         unoptimized: true
