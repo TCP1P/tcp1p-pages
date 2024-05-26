849a1999115b250ac1371d412976ff144e2fa6e5
dimas <dimasmaulana0305@gmail.com>
2024-05-26 17:37:51 +0800

    add seccodeid

--- src/app/tcp1pctf-2024/sponsors.tsx
+++ src/app/tcp1pctf-2024/sponsors.tsx
@@ -1,20 +1,39 @@
 import Image from "next/image"
 import GoogleImage from "../../../public/Google-Cloud-Emblem.png"
+import SeccodeId from "../../../public/secodeid.jpeg"
+import Link from "next/link"
 
 export function Sponsor() {
     const sponsors = [
         {
             name: "Google",
             text: "Infra sponsored by goo.gle/ctfsponsorship",
-            image: GoogleImage
+            image: GoogleImage,
+            url: "https://goo.gle/ctfsponsorship"
         },
+        {
+            name: "Seccodeid Forum",
+            text: "Seccodeid Forum is a community of cybersecurity enthusiasts in Indonesia.",
+            image: SeccodeId,
+            url: "https://seccodeid.com/"
+        }
     ]
     return <>
-        <div className="grid justify-center lg:grid-cols-3 md:grid-cols-2 gap-4">
-            {sponsors.map((sponsor, _index) => (
-                <div className="card w-96">
-                    <figure className="px-10 pt-10">
-                        <Image src={sponsor.image} alt={sponsor.text}></Image>
+        <div className="grid justify-center lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+            {sponsors.map((sponsor, index) => (
+                <div key={index} className="card shadow-lg">
+                    <figure className="px-10 pt-10 w-full flex justify-center">
+                        <Link href={sponsor.url} target="_blank">
+                            <div className="w-[250px] h-[250px] relative">
+                                <Image
+                                    src={sponsor.image}
+                                    alt={sponsor.name}
+                                    layout="fill"
+                                    objectFit="contain"
+                                    quality={100}
+                                />
+                            </div>
+                        </Link>
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">{sponsor.name}</h2>
