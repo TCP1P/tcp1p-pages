04840102ad6c96466db3a1f0c2e88547d1159b5a
dimas <dimasmaulana0305@gmail.com>
2024-10-01 08:02:36 +0800

    change tcp1p moto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return <>
     <div className="text-center">
-      TCP1P is an Indonesian CTF team actively engaging in competitive cybersecurity events on ctftime.
+      TCP1P is Indonesian CTF community dedicated to organizing engaging Capture The Flag events and collaborating with local competitions. Our mission is to elevate the quality of CTF challenges in Indonesia and foster a thriving cybersecurity ecosystem through knowledge sharing.
     </div>
   </>
 }
